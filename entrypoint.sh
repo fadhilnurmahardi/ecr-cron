973320db0d627dbf5f96770453b78e01f06dba35
Fadhil Nur Mahardi <fadhilnurmahardi49@gmail.com>
2021-06-28 14:58:18 +0700

    chore(docker-image): ecr cron

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+REGION=$AWS_DEFAULT_REGION
+SECRET_NAME=${REGION}-ecr-registry
+EMAIL=$EMAIL_ECR
+TOKEN=`aws ecr get-login --region ${REGION} --registry-ids ${ACCOUNT} | cut -d' ' -f6`
+echo "ENV variables setup done."
+kubectl -n ${KUBE_NAMEPSACE} delete secret --ignore-not-found $SECRET_NAME
+kubectl -n ${KUBE_NAMEPSACE} create secret docker-registry $SECRET_NAME \
+--docker-server=https://${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com \
+--docker-username=AWS \
+--docker-password="${TOKEN}" \
+--docker-email="${EMAIL}"
+echo "Secret created by name. $SECRET_NAME"
